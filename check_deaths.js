4b7602785975a9ec60b7283fc9e4b3c5f758a2e3
patitokun03-spec <patitokun03@gmail.com>
2026-06-03 20:13:27 +0200

    Remove irrelevant files (debug scripts, OperaSetup.exe) and update .gitignore

--- check_deaths.js
+++
@@ -1,7 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./database.sqlite');
-
-db.run("DELETE FROM logs_cache", (err) => {
-    if (err) throw err;
-    console.log("Cache cleared");
-});
